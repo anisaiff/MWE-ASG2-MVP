16d717ce9a83674efbbc551f2a62b36cf86d3f04
anisah <22ftt1509@student.pb.edu.bn>
2024-05-23 20:48:53 +0800

    Refactor login page routing and module imports

--- BinRadar/src/app/login/login-routing.module.ts
+++ BinRadar/src/app/login/login-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginPage } from './login.page';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LoginPage
-  }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
+  { path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule) }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
 })
-export class LoginPageRoutingModule {}
+export class AppRoutingModule { }
