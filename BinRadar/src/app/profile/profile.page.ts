e0ad83238ea2e6c636a71f87e48a1d8ede296d4d
anisah <22ftt1509@student.pb.edu.bn>
2024-05-24 21:46:09 +0800

    feat: Update profile page layout and styling

--- BinRadar/src/app/profile/profile.page.ts
+++ BinRadar/src/app/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  constructor(private navCtrl: NavController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  logout() {
+    // Implement your logout logic here (navigation, service call, etc.)
+    console.log('Logging out...');
+    // Maybe navigate back to login or another page
+    // this.navCtrl.navigateBack('/login');
   }
-
 }
