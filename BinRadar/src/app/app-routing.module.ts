1eba0d7c5207e9cee77f413c1ae81e28f1b92154
anisah <22ftt1509@student.pb.edu.bn>
2024-05-25 08:08:08 +0800

    Refactor routing configuration for login, signup, and home pages

--- BinRadar/src/app/app-routing.module.ts
+++ BinRadar/src/app/app-routing.module.ts
@@ -2,24 +2,34 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
-  { path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule) },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full' // Redirect to 'login' when no path is specified
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule)
+  },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: 'level-list',
-    loadChildren: () => import('./level-list/level-list.module').then( m => m.LevelListPageModule)
+    loadChildren: () => import('./level-list/level-list.module').then(m => m.LevelListPageModule)
   },
   {
-    path: 'level-details',
-    loadChildren: () => import('./level-details/level-details.module').then( m => m.LevelDetailsPageModule)
+    path: 'level-detail',
+    loadChildren: () => import('./level-details/level-details.module').then(m => m.LevelDetailsPageModule)
   },
   {
     path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
   },
 ];
 
